2384dc1f449cc526a20bdf14c48e2f19d307e84a
WeronikaMarciniak <47399808+WeronikaMarciniak@users.noreply.github.com>
2020-01-14 18:32:49 +0100

    Update drivingplate.py with edge detection
    The file drivingplate.py has been updated with adding Canny edge detection method

--- drivingplate.py
+++ drivingplate.py
@@ -1,20 +1,23 @@
-
-#import numpy as np
+import numpy as np
 import cv2
-#import matplotlib.pyplot as plt
 
-video = cv2.VideoCapture('CarLicensePlate.mp4')
+blur=21   # bluring value
 
-while(video.isOpened()):
-    ret, frame = video.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+cap = cv2.VideoCapture('video6.mp4') #video3,video6,carlicenseplate - license plate is recognized, when Canny algorithm is used and when the angle of plate's view is none or very little 
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read() 
+  # Our operations on the frame come here
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #converting to gray scale
+    eq = cv2.equalizeHist(gray) #histogram equalization
+    frame = cv2.GaussianBlur(frame, (blur,blur), 0, 0) #Gaussian Blur
+    frame = cv2.Canny(eq,20,60,1)  # Canny function
+   
+    cv2.imshow('Video',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
         break
 
-video.release()
+cap.release()
 cv2.destroyAllWindows()
 
 
